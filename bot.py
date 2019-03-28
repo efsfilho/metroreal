f51f3e0d59189e756e58d2898510068178856ca6
efsfilho <efdsfilho@gmail.com>
2019-03-28 17:58:35 -0300

    dev update

--- bot.py
+++ bot.py
@@ -19,19 +19,36 @@ dispatcher = updater.dispatcher
 # w = Watcher()
 w = Watcher(interval=5, startTime='17:31', stopTime='17:50', rushInterval=2,
             rushStartTime='17:45', rushStopTime='17:50')
-w.start()
+# w.start()
+
+from functools import wraps
+LIST_OF_ADMINS = [] # usuario permitido
+
+# Filtro de usuário
+def restricted(func):
+    @wraps(func)
+    def wrapped(update, context, *args, **kwargs):
+        # user_id = context.message.chat.id
+        user = context.message.chat
+        if user.id not in LIST_OF_ADMINS:
+            logging.info(f'Unauthorized access denied for {user}.')
+            return
+        return func(update, context, *args, **kwargs)
+    return wrapped
+
 
 def status(bot, update):
     # TODO try
     status = w.getStatus()
     bot.send_message(chat_id=update.message.chat_id, text=status)
 
+@restricted
 def eco(bot, update):
-  logging.debug('eco debug');
-  bot.send_message(chat_id=update.message.chat_id, text='ecossss')
+    logging.debug('eco debug');
+    bot.send_message(chat_id=update.message.chat_id, text='ecossss')
 
 def ini(bot, update):
-  logging.info('ininin');
+    logging.info('ininin');
 
 dispatcher.add_handler(CommandHandler('eco', eco))
 dispatcher.add_handler(CommandHandler('status', status))
